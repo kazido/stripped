a4caa1f79b8a655b78d33f611b2578b54edf8645
bry <beegee1803@gmail.com>
2026-04-10 03:58:42 -0400

    removed entity and moved enemies

--- game/entities/enemy.py
+++ game/entities/enemy.py
@@ -1,9 +1,12 @@
 import random
 from abc import ABC
 from enum import Enum
-from .base import Entity
-from .player import Player
 
+from .player import Player
+from game.battles.io import IOHandler
+
+
+# region Enemy Base Class
 
 class Nature(Enum):
     CAREFREE = -2
@@ -11,29 +14,82 @@ class Nature(Enum):
     GIANT = 10
 
 
-class Enemy(Entity, ABC):
+class Enemy(ABC):
     # Default stats, no need to add them to instanced object
-    base_hp: int = 0
-    base_speed: int = 0
-    damage: int = 0
+    NAME: str
+    BASE_HP: int = 0
+    BASE_SPEED: int = 0
+    DAMAGE: int = 0
 
     def __init__(self) -> None:
+        # Pick a nature
         self.nature = random.choice(list(Nature))
-        super().__init__()
 
-    @property
-    def max_hp(self) -> int:
-        # TODO: Handle any HP modifiers (like nature)
-        return self.base_hp + self.nature.value
-    
-    @property
-    def speed(self) -> int:
-        # TODO: Handle any speed modifiers (like nature)
-        return self.base_speed
+        # Prepare battle state
+        self.current_hp = max(1, self.BASE_HP + self.nature.value)
+        self.speed = self.BASE_SPEED
     
     def __str__(self) -> str:
-        return self.nature.name.capitalize()
+        return self.nature.name.capitalize() + self.NAME
+    
 
-    def get_players(self, targets: list[Entity]) -> list[Player]:
-        """Get a list of all alive player targets in the battle."""
-        return [t for t in targets if isinstance(t, Player) and not t.is_dead]
+    def take_turn(self, players: list[Player], handler: IOHandler) -> None:
+        pass
+    
+
+    def pre_battle(self) -> None:
+        """Anything that needs to happen at the start of battles (special messages)."""
+        pass
+
+    def post_battle(self) -> None:
+        """Anything that needs to happen at the end of battles (special messages)."""
+        pass
+    
+# endregion
+
+
+# region Enemies
+
+
+class Goblin(Enemy):
+    NAME = "Goblin"
+    BASE_HP = 10
+    BASE_SPEED = 8
+    DAMAGE = 3
+
+    def take_turn(self, players, handler) -> None:
+        # Goblins always attack the player with the most gold.
+        richest = max(players, key=lambda p: p.gold)
+        handler.output(f"{self} is eyeing {richest}...")
+
+
+class Centaur(Enemy):
+    NAME = "Centaur"
+    BASE_HP = 50
+    BASE_SPEED = 25
+    DAMAGE = 15
+
+    def take_turn(self, players, handler) -> None:
+        pass
+
+
+class UnstableDrone(Enemy):
+    """After three turns, the Mazhinx will self-destruct."""
+    name = "Unstable Drone"
+    BASE_HP = 100
+    BASE_SPEED = 1
+    DAMAGE = 100
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.turns_to_destruct: int = 3
+
+    def take_turn(self, players, handler) -> None:
+        self.turns_to_destruct -= 1
+        if self.turns_to_destruct <= 0:
+            for player in players:
+                if player.take_damage(self.DAMAGE):
+                    handler.output(f"{str(player)} has died.")
+
+
+# endregion
